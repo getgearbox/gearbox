976038d8187c47af3bfda933a2de8a57f623183c
Anindya Sinha <anindya_sinha@apple.com>
2013-12-19 07:18:45 +0000

    Scripts for install and uninstall of rpms added.
    With the environment variabled PKG_VERSION and PKG_RELEASE, these scripts
    installs and uninstalls the gearbox rpms. These scripts shall be used with
    the jenkins job for running smoke tests after commits.

---
+++ gearbox-uninstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script uninstalls the gearbox rpms. The PKG_VERSION and PKG_RELEASE environment
+# variables need to be set appropriately.
+
+LIST_RPM=("core" "perl-lib" "daemons" "test-workers-config" "job-devel" "job" "worker" \
+          "status-plugin-sql" "svc-builder" "daemons-config" "test-workers" "tools" \
+          "store-devel" "store" "dbsetup" "python-lib" "worker-devel" "core-devel")
+
+for rpm_suffix in ${LIST_RPM[@]}; do
+    sudo yum erase -y gearbox-$rpm_suffix-$PKG_VERSION-$PKG_RELEASE.el6.x86_64
+done
+sudo yum erase -y mod_gearbox-$PKG_VERSION-$PKG_RELEASE.el6.x86_64
