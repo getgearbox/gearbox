9b46c71f3a9b27f4d21512dca96eae3d820b16a4
Alexius Ludeman <ludeman@apple.com>
2014-01-08 23:00:29 -0800

    optional param to run a single test directory

--- workers/run-tests.sh
+++ workers/run-tests.sh
@@ -3,11 +3,12 @@
 #
 set -e
 
-SCRIPTS=$0
+SCRIPT=$0
 LANGUAGES=(cpp perl python)
 
 usage() {
-    echo "Usage: $SCRIPT [--languages \"cpp perl python\"] [--host gearbox]"
+    echo "Usage: $SCRIPT [--languages \"cpp perl python\"][--host gearbox]"
+    echo "       [--test test-agents]"
     echo "    --languages defaults to \"$LANGUAGES\""
     exit 1
 }
@@ -16,6 +17,7 @@ while [[ $# -ge 1 ]]; do
     case $1 in
         --languages) shift; LANGUAGES=($=1); shift ;;
         --host) shift ; export SMOKE_HOST=$1 ; shift ;;
+        --test) shift ; TESTDIR=$1 ; shift ;;
         *) echo "Unknown option \"$1\""; usage ;;
     esac
 done
@@ -27,6 +29,12 @@ for language in $LANGUAGES; do
         export PREFIX=$language
     fi
 
-    find . -name 'test.t' -print0 | \
+    if [[ -z $TESTDIR ]]; then
+        BASE="."
+    else
+        BASE=$TESTDIR
+    fi
+
+    find $BASE -name 'test.t' -print0 | \
         xargs --null -L1 zsh -c 'echo $0 ; cd $0:h && ./$0:t'
 done
